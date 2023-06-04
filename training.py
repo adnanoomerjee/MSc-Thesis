a3feb440a6f6d97122930838ba13c5363dfab917
Adnan <55955241+adnanoomerjee@users.noreply.github.com>
2023-06-04 22:37:29 +0100

    cluster test

---
+++ training.py
@@ -0,0 +1,33 @@
+import functools
+import jax
+import os
+
+from datetime import datetime
+from jax import numpy as jp
+import matplotlib.pyplot as plt
+
+from IPython.display import HTML, clear_output
+
+try:
+  import brax
+except ImportError:
+  !pip install git+https://github.com/google/brax.git@main
+  clear_output()
+  import brax
+
+import flax
+from brax import envs
+from brax.io import model
+from brax.io import json
+from brax.io import html
+from ppo import train as ppo
+from brax.training.agents.sac import train as sac
+
+env_name = 'ant'  # @param ['ant', 'halfcheetah', 'hopper', 'humanoid', 'humanoidstandup', 'inverted_pendulum', 'inverted_double_pendulum', 'pusher', 'reacher', 'walker2d']
+backend = 'positional'  # @param ['generalized', 'positional', 'spring']
+
+env = envs.get_environment(env_name=env_name,
+                           backend=backend)
+state = jax.jit(env.reset)(rng=jax.random.PRNGKey(seed=0))
+
+HTML(html.render(env.sys, [state.pipeline_state]))
