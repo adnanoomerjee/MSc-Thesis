ca68e0d072cb8a8bc8bd019abd7af62d4edb599f
Adnan <55955241+adnanoomerjee@users.noreply.github.com>
2023-06-06 01:15:02 +0100

    modify training.ipynb

--- hpc-custom-setup.sh
+++ hpc-custom-setup.sh
@@ -1,5 +1,7 @@
 cd /nfs/nhome/live/aoomerjee
 export HOME=/nfs/nhome/live/aoomerjee
-srun -p gpu --gres=gpu:1 -w gpu-380-15 --pty /bin/bash
+srun -p gpu --gres=gpu:2 -w gpu-380-15 --exclusive --pty /bin/bash -l
+srun -p gpu --gres=gpu:4 -w gpu-sr670-21 --exclusive --pty /bin/bash -l
+
 source .bashrc
 conda activate msc-thesis-hpc
