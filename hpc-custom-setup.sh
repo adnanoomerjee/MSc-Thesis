29099aab13db41271caa0b5d7ec41281cd04b271
Adnan <55955241+adnanoomerjee@users.noreply.github.com>
2023-06-06 01:22:38 +0100

    add gitignore

--- hpc-custom-setup.sh
+++
@@ -1,7 +0,0 @@
-cd /nfs/nhome/live/aoomerjee
-export HOME=/nfs/nhome/live/aoomerjee
-srun -p gpu --gres=gpu:2 -w gpu-380-15 --exclusive --pty /bin/bash -l
-srun -p gpu --gres=gpu:4 -w gpu-sr670-21 --exclusive --pty /bin/bash -l
-
-source .bashrc
-conda activate msc-thesis-hpc
