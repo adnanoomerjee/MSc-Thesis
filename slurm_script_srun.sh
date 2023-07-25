6dac1d086c8881de1cb9a7bd6c8f540242bb8bc3
adnanoomerjee <adnan.oomerjee@outlook.com>
2023-07-25 13:58:58 +0000

    updates

--- slurm_script_srun.sh
+++ slurm_script_srun.sh
@@ -1,4 +1,4 @@
-srun --job-name=hct_run --nodes=1 --partition=fast --ntasks-per-node=1 --gres=gpu:1 --exclusive --pty /bin/bash -l
+srun --job-name=hct_run --nodes=1 --partition=fast --ntasks-per-node=1 --gres=gpu:1 --pty /bin/bash -l
 
 source /nfs/nhome/live/aoomerjee/mambaforge/bin/activate
 conda activate msc-thesis-hpc
